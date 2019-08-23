c72df2010e799bbae63b7eb33bc09d11f7793fdb
96 <43505842+96Akrome@users.noreply.github.com>
2019-08-23 15:58:57 -0400

    Add files via upload
    se elimino funcion getIndex porq existe lista.index(item), se empezo el archivo update.py, voy a cocinar, mas rato vuelvo uwu

---
+++ Update.py
@@ -0,0 +1,49 @@
+import re
+import collections
+
+'''
+Nombre de la función: cleanValues
+——————–
+Entradas:
+(lista group_list) Lista que contiene los valores obtenidos por el match entre el query y la expresión regular
+——————–
+Salida:
+(lista group_list) Misma lista ingresada, pero sin espacios blancos ni apóstrofes al inicio y final de cada valor
+——————–
+Descripción de la función:  Itera con los valores de la lista ingresada limpiando los espacios blancos y
+                            apóstrofes al inicio y al final de cada valor usando la concatenación
+                            del comando value.strip().strip("'").strip()
+'''
+#Problema con esta función: si alguien decide trollear y decir que el nombre de algo es, por ejemplo
+# columna = "col'", al usar strip("'") estaria modificando el nombre real y realizando una comparación
+# erronea, TODO in the future.
+def cleanValues(group_list):
+    i = 0
+    while (i < len(group_list)):
+        group_list[i] = group_list[i].strip().strip("'").strip()
+        i = i + 1
+    return group_list
+
+def getDict(lista1, lista2):
+    i = 0
+    dicti = dict()
+    while(i < len(lista1)):
+        dicti[lista1[i]] = lista2[i]
+        i = i+1
+    return dicti
+
+update_Regex = r"UPDATE(.+)SET(.+)WHERE(.+);"
+
+valid = re.compile(update_Regex)
+
+
+
+print ("ingrese UPDATE: ")
+statement = input()
+
+Tabla = valid.match(statement).groups()[0].strip().strip("'").strip()
+Set = cleanValues(re.split(r',', valid.match(statement).groups()[1]))
+Where = cleanValues(re.split(r',', valid.match(statement).groups()[2]))
+
+
+
--- insert.py
+++ insert.py
@@ -1,51 +1,32 @@
 import re
 import collections
 
-def removeWhiteSpaces(group_list):
+def cleanValues(group_list):
     i = 0
     while (i < len(group_list)):
-        group_list[i] = group_list[i].strip()
+        group_list[i] = group_list[i].strip().strip("'").strip()
         i = i + 1
     return group_list
 
-def getIndex(lista, value):
-    i = 0
-    for dato in lista:
-        if(dato.strip() == value):
-            return i
-        i = i + 1
-    return -1
-
-def getDict(columnas, values):
+def getDict(lista1, lista2):
     i = 0
     dicti = dict()
-    while(i < len(columnas)):
-        dicti[columnas[i]] = values[i]
+    while(i < len(lista1)):
+        dicti[lista1[i]] = lista2[i]
         i = i+1
     return dicti
 
-insert_Regex = r" *INSERT +INTO +(.+) *\( *(.+) *\) *VALUES *\( *( *.+ *) *\);"
+insert_Regex = r"INSERT +INTO(.+)\((.+)\) *VALUES *\((.+)\) *;"
 
 valid = re.compile(insert_Regex)
 
 print ("ingrese INSERT: ")
 statement = input()
 
-tabla = re.split(r',', valid.match(statement).groups()[0])
-columnas = re.split(r',', valid.match(statement).groups()[1])
-values = re.split(r',', valid.match(statement).groups()[2])
+tabla = cleanValues(re.split(r',', valid.match(statement).groups()[0]))
+columnas = cleanValues(re.split(r',', valid.match(statement).groups()[1]))
+values = cleanValues(re.split(r',', valid.match(statement).groups()[2]))
 
-tabla = removeWhiteSpaces(tabla)
-columnas = removeWhiteSpaces(columnas)
-values = removeWhiteSpaces(values)
-
-print("tabla: ")
-print (tabla)
-print("\ncolumnas")
-print(columnas)
-print("\nValues")
-print(values)
-print(tabla[0])
 if(len(columnas) != len(values)):
     print("error de syntax")
 
@@ -56,17 +37,13 @@ else:
     #file test
     file = open(tabla[0] + ".csv", "r+")
     indices = []
-    columnasFile = file.readline()
-    print ("las columnas del archivo son:")
-    print (columnasFile)
+    columnasFile = file.readline().split(",")
+
     for coso in columnas:
-        indice = getIndex(columnasFile.split(","), coso)
-        if(indice != -1):
-            indices.append(indice)
+        if(coso in columnasFile):
+            indices.append(columnasFile.index(coso))
+
     index = getDict(indices, columnas)
-    print("indices")
-    print(indices)
-    print(index)
     output = []
 
     i = 0
