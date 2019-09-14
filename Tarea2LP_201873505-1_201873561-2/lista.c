462655f81d632d54a7a041f45006b015039bf0cb
argentumx <miffyrli@gmail.com>
2019-09-14 17:20:27 -0300

    clear,init

--- Tarea2LP_201873505-1_201873561-2/lista.c
+++ Tarea2LP_201873505-1_201873561-2/lista.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
 #include "lista.h"
+
+
+void init(struct lista *a){
+  struct lista *a = (struct lista*)malloc(sizeof(struct lista));
+  a->actual = NULL;
+  a->head = NULL;
+  a->length = 0;
+  a->tail = NULL;
+  return;
+}
+
+//no soporta listas encrustradas todavia.
+void clear(struct lista *a){
+  struct nodo *loop_aux_next;
+  a->actual = a->head;
+  while(a->head != NULL){
+    loop_aux_next = a->head;
+    a->head = a->head->next;
+    free(loop_aux_next);
+  }
+  a->length = 0;
+  a->actual = NULL;
+  a->tail = NULL;
+}
