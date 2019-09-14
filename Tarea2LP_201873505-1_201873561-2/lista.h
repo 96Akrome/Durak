f5b90420a711c05c53f1ef8f3615f6d01c8a0ba1
argentumx <miffyrli@gmail.com>
2019-09-14 17:07:54 -0300

    lista.h como en pdf, preguntar.

---
+++ Tarea2LP_201873505-1_201873561-2/lista.h
@@ -0,0 +1,25 @@
+//structs mal escritos en pdf. preguntar o hacer typedef.
+struct nodo{
+  struct dato info;
+  struct nodo* next;
+}algo1;
+
+struct lista{
+  struct nodo* actual;
+  struct nodo* head;
+  struct nodo* tail;
+  int length;
+}algo2;
+
+struct dato{
+  void* contenido;
+  char tipo;
+}algo3;
+
+void init(struct lista *a);
+void clear(struct lista *a);
+void insert(struct lista *a,int i,dato d);
+void append(struct lista *a, dato d);
+void remove(struct lista *a,int i);
+int length(strcut lista *a);
+dato* at(struct lista *a,int i);
