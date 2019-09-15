ac504db488537745059a1bc9f659b9f26dec259e
argentumx <miffyrli@gmail.com>
2019-09-15 20:07:28 -0300

    errores de tipeo de lista.h

--- Tarea2LP_201873505-1_201873561-2/lista.h
+++ Tarea2LP_201873505-1_201873561-2/lista.h
@@ -1,4 +1,4 @@
-//structs mal escritos en pdf. preguntar o hacer typedef.
+
 struct nodo{
   struct dato info;
   struct nodo* next;
@@ -20,6 +20,6 @@ void init(struct lista *a);
 void clear(struct lista *a);
 void insert(struct lista *a,int i,struct dato d);
 void append(struct lista *a, struct dato d);
-void remove(struct lista *a,int i);
+void remov(struct lista *a,int i);
 int length(struct lista *a);
 dato* at(struct lista *a,int i);
