e0f7c3a057a00881a75a47d068807e48899a4e6c
argentumx <miffyrli@gmail.com>
2019-09-21 16:30:19 -0300

    revision lista.c

--- Tarea2LP_201873505-1_201873561-2/lista.c
+++ Tarea2LP_201873505-1_201873561-2/lista.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lista.h"
 
-
 void init(struct lista *a){
-  a = (struct lista*)malloc(sizeof(struct lista));
-  a->actual = NULL;
-  a->head = NULL;
-  a->length = 0;
-  a->tail = NULL;
-  return;
+    //a = (struct lista*)malloc(sizeof(struct lista));
+    a->actual = NULL;
+    a->head = NULL;
+    a->length = 0;
+    a->tail = NULL;
+    return;
 }
 
 //hay que probar para listas incrustadas, deberia funcionar
@@ -17,168 +14,204 @@ void init(struct lista *a){
 //uso a->actual en vez de nodo loop_aux porque igual despues se asigna como NULL
 //en realidad es un nodo inutil en struct sjsjjsjsjs como no hay ni next ni prev
 void clear(struct lista *a){
-  while(a->head != NULL){
-    a->actual = a->head;
-    if(a->actual->info.tipo != 'l'){
-      //no es un nodo que es a la vez cabeza de una lista
-      a->head = a->head->next;
-      //se libera el contenido de datos
-      free(a->actual->info.contenido);
-      //se libera el nodo
-      free(a->actual);
+    while(a->head != NULL){
+        a->actual = a->head;
+        a->head = a->head->next;
+        if(strcmp(a->actual->info.tipo, "l") == 0){
+          clear((struct lista*)a->actual->info.contenido);
+        }
+        free(a->actual->info.contenido); // Se libera el contenido de datos
+        free(a->actual); // Se libera el nodo
     }
-    else{
-      //recursion
-      clear((struct lista*)a->actual);
-    }
-  }
-  a->length = 0;
-  a->actual = NULL;
-  a->tail = NULL;
-  //a->head queda como NULL en el loop.
-  return;
-}
-
-void append(struct lista *a, struct dato d){
- struct nodo *aux = (struct nodo*)malloc(sizeof(struct nodo));
- aux->info = d;
- aux->next = NULL;
-
- if (a->head ==  NULL){
-   a->head =  aux;
-   a->tail = aux;
-   a->actual = aux;
- }
- else{
-   a->tail->next = aux;
-   a->tail = aux;
- }
- a->length++;
+    init(a);
+    return;
 }
 
 void insert(struct lista *a, int i, struct dato d){
-  if (i == 0 && a->length == 0){
-    printf("La lista es vacia. Llamando al append.\n");
-    append(a,d);
-    return;
-  }
-  if(i ==  a->length){
-    printf("Se requiere insertar al final de la lista.\n");
-    append(a,d);
-    return;
-  }
-  if (i > a->length || i < 0){
-    printf("No se puede insertar,indice out of bounds.\n");
-    return;
-  }
-  struct nodo *aux = (struct nodo*)malloc(sizeof(struct nodo));
-  aux->info = d;
-  if(i == 0 && a->length != 0){
-    printf("Se requiere insertar al inicio en lista no vacia.\n");
-    aux->next = a->head;
-    a->head = aux;
-    a->length++;
-  }
-  else{
-    printf("Se requiere insertar en posicion %d de la lista no vacia.\n",i);
-    int k;
-    struct nodo *loop_aux;
-    loop_aux = a->head;
-    for(k = 0; k < i-1; k++){
-      loop_aux = loop_aux->next;
+    if (i == 0 && length(a) == 0){
+        printf("La lista es vacia. Llamando al append.\n");
+        append(a,d);
+        return;
     }
-    aux->next = loop_aux->next;
-    loop_aux->next = aux;
+    if(i ==  length(a)){
+        printf("Se requiere insertar al final de la lista.\n");
+        append(a,d);
+        return;
+    }
+    if (i > length(a) || i < 0){
+        printf("No se puede insertar,indice out of bounds.\n");
+        // TODO considerar borrar linea 54 y 55, hay que conocer el struct que ingresaran para saber si limpiar o no esa memoria.
+        if(strcmp(d.tipo, "l") == 0){clear((struct lista *)d.contenido);}
+        free(d.contenido);
+        return;
+    }
+    struct nodo *aux = (struct nodo*)malloc(sizeof(struct nodo));
+    aux->info = d;
+    if(i == 0 && length(a) != 0){
+        printf("Se requiere insertar al inicio en lista no vacia.\n");
+        aux->next = a->head;
+        a->head = aux;
+        a->length++;
+    }
+    else{
+        printf("Se requiere insertar en posicion %d de la lista no vacia.\n",i);
+        int k;
+        a->actual = a->head;
+        for(k = 0; k < i-1; k++){
+          a->actual = a->actual->next;
+        }
+        aux->next = a->actual->next;
+        a->actual->next = aux;
+        a->length++;
+    }
+    return;
+}
+
+void append(struct lista *a, struct dato d){
+    struct nodo *aux = (struct nodo*)malloc(sizeof(struct nodo));
+    aux->info = d;
+    aux->next = NULL;
+    if (a->head ==  NULL){
+        a->head =  aux;
+        a->tail = aux;
+        a->actual = aux;
+    }
+    else{
+        a->tail->next = aux;
+        a->tail = aux;
+    }
     a->length++;
-  }
-  return;
 }
 
 void remov(struct lista *a, int i){
-  if((a->length == 0) | (i >= a->length) || (i < 0)){
-    printf("La lista es vacia o el indice esta out of bounds.\n");
-    return;
-  }
-  struct nodo *aux;
-  //al inicio de la lista
-  if(i == 0){
-    printf("Se requiere borrar el primer elemento de la lista no vacia.\n");
-    //reasigno la cabeza, pero guardo el puntero a esta en aux.
-    aux = a->head;
-    a->head = aux->next;
-    //cualquier tipo no recursivo (entero o flotante)
-    if(a->head->info.tipo != 'l'){
-      //limpio el contenido
-      free(aux->info.contenido);
-      //libero el nodo
-      free(aux);
+    if((a->length == 0) | (i >= a->length) || (i < 0)){
+        printf("La lista es vacia o el indice esta out of bounds.\n");
+        return;
     }
+    struct nodo *aux;
+    //al inicio de la lista
+    if(i == 0){
+        printf("Se requiere borrar el primer elemento de la lista no vacia.\n");
+        //reasigno la cabeza, pero guardo el puntero a esta en aux.
+        aux = a->head;
+        a->head = aux->next;
+        //cualquier tipo no recursivo (entero o flotante)
+        if(a->head->info.tipo != 'l'){
+            //limpio el contenido
+            free(aux->info.contenido);
+            //libero el nodo
+            free(aux);
+        }
+        else{
+            //tipo recursivo, lista incrustada (posiblemente con otras incrustadas)
+            clear((struct lista*)aux);
+            //no tenemos que hacer mas frees, dado que clear lo hara solo.
+        }
+        a->length--;
+        return;
+    }
+    struct nodo *loop_aux;
+    loop_aux = a->head;
+    int k;
+    //recorre hasta i-1 para poder borrar la cola
+    for(k = 0; k < i-1; k++){
+      loop_aux = loop_aux->next;
+    }
+    //eliminar al final (largo es siempre +1 nodo, es cantidad y no total de posiciones.)
+    if(i == a->length - 1){
+        printf("Se requiere borrar al final de la lista.\n");
+        aux = loop_aux->next;
+        loop_aux->next = NULL;
+        a->tail = loop_aux;
+        if(aux->info.tipo != 'l'){
+          free(aux);
+        }
+        else{
+          clear((struct lista*)aux);
+        }
+        a->length--;
+        return;
+    }
+    //eliminar dentro pero no el el extremo
     else{
-      //tipo recursivo, lista incrustada (posiblemente con otras incrustadas)
-      clear((struct lista*)aux);
-      //no tenemos que hacer mas frees, dado que clear lo hara solo.
+        printf("Se requiere borrar en la posicion %d\n",i );
+        aux = loop_aux->next;
+        loop_aux->next = loop_aux->next->next;
+        if(aux->info.contenido != 'l'){
+            free(aux);
+        }
+        else{
+            clear((struct lista*)aux);
+        }
+        a->length--;
+        return;
     }
-    a->length--;
     return;
-  }
-  struct nodo *loop_aux;
-  loop_aux = a->head;
-  int k;
-  //recorre hasta i-1 para poder borrar la cola
-  for(k = 0; k < i-1; k++){
-    loop_aux = loop_aux->next;
-  }
-  //eliminar al final (largo es siempre +1 nodo, es cantidad y no total de posiciones.)
-  if(i == a->length-1){
-    printf("Se requiere borrar al final de la lista.\n");
-    aux = loop_aux->next;
-    loop_aux->next = NULL;
-    a->tail = loop_aux;
-    if(aux->info.tipo != 'l'){
-      free(aux);
-    }
-    else{
-      clear((struct lista*)aux);
-    }
-    a->length--;
-    return;
-  }
-  //eliminar dentro pero no el el extremo
-  else{
-    printf("Se requiere borrar en la posicion %d\n",i );
-    aux = loop_aux->next;
-    loop_aux->next = loop_aux->next->next;
-    if(aux->info.tipo != 'l'){
-      free(aux);
-    }
-    else{
-      clear((struct lista*)aux);
-    }
-    a->length--;
-    return;
-  }
-  return;
 }
+
+/*
+void remov2(struct lista *a, int i){ // Propuesta de remov, falta revision
+    if((a->length == 0) | (i >= a->length) || (i < 0)){
+        printf("La lista es vacia o el indice esta out of bounds.\n");
+        return;
+    }
+    if((a -> length == 1) && (i == 0)){
+        if(strcmp(a->head->info.tipo, "l") == 0){
+          clear((struct lista*)a->head->info.contenido);
+        } // Si el nodo era una lista lo borra con la funcion clear
+        free(a->head->info.contenido); // limpia el contenido
+        free(a->head); // libera el nodo
+        init(a);
+        return;
+    }
+    struct nodo *aux; // aux almacenará el nodo a remover.
+    if(i == 0){ // Al inicio de la lista
+        printf("Se requiere borrar el primer elemento de la lista no vacia.\n");
+        aux = a->head;
+        a->head = a->actual->next; // Le asigno a head el puntero hacia el siguiente nodo.
+    }
+    else{
+        a->actual = a->head; // iterador de la lista
+        int k;
+        for(k = 0; k < i - 1; k++){
+          a->actual = a->actual->next;
+        } // Itera hasta llegar a la posición (i - 1)
+        aux = a->actual->next; // Al salir del for, aux será el elemento a remover (posicion i)
+        if(i == a->length - 1){ // La cola de la lista
+            printf("Se requiere borrar el elemento en el final de la lista.\n");
+            a->tail = a->actual;
+        }
+        else{ // Posición cualquiera de la lista
+            printf("Se requiere borrar el elemento en la posicion %d.\n",i );
+            a->actual->next = aux->next;
+        }
+    }
+    if(strcmp(aux->info.tipo, "l") == 0){
+      clear((struct lista*)aux->info.contenido);
+    } // Si el nodo era una lista lo borra con la funcion clear
+    free(aux->info.contenido); // limpia el contenido
+    free(aux); // libera el nodo
+    a->length--;
+    return;
+}*/
 
 int length(struct lista *a){
-  return a->length;
+    return a->length;
 }
 
-
 struct dato* at(struct lista *a, int i){
-  if((a->length == 0) | (i >= a->length) || (i < 0)){
-    printf("La lista esta vacia o el dato esta fuera de rango.\n");
-  }
+    if((length(a) == 0) | (i >= length(a)) || (i < 0)){
+        printf("La lista esta vacia o el dato esta fuera de rango.\n");
+    }
   //caso 1: nodo en la cabeza, retorna directamente
-  if(i == 0){
-    return &(a->head->info);
-  }
-  struct nodo *loop_aux;
-  loop_aux = a->head;
-  int k;
-  //recorre hasta posicion i en la lista.
-  for(k = 0; k < i; k++){
-    loop_aux = loop_aux->next;
-  }
-  return &(loop_aux->info);
+    if(i == 0){
+        return &(a->head->info);
+    }
+    a->actual = a->head;
+        int k;
+    //recorre hasta posicion i en la lista.
+    for(k = 0; k < i; k++){
+        a->actual = a->actual->next;
+    }
+    return &(a->actual->info);
 }
