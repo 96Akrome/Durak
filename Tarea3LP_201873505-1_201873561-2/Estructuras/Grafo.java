39ffd93503b861d08e69865fe1f4dd211d1dabf8
argentumx <miffyrli@gmail.com>
2019-10-20 14:25:45 -0300

    .

--- Tarea3LP_201873505-1_201873561-2/Estructuras/Grafo.java
+++ Tarea3LP_201873505-1_201873561-2/Estructuras/Grafo.java
@@ -1,9 +1,57 @@
+import java.util.List;
+import java.util.LinkedList;
+
 interface Grafo{
     int nNodes = 0;
     int nEdges = 0;
-
+    //List<Vertex> vertexes;
+    List<Edge> adjList ;
+    //lista de nodos
     //float edgeWeight(int,int);
     //List<T> shortestPath(int,int);
     //void addEdge(int,int,int);
     //addNode(int);
+}
+
+//vertice con su numero ( == nodo)
+class Vertex{
+    final private int id;
+    public Vertex(int id) {
+        this.id = id;
+    }
+    public String getId() {
+        return id;
+    }
+}
+
+//arcos con inicio,fin,peso
+class Edge{
+    private final int id;
+    private final Vertex source;
+    private final Vertex dest;
+    private final int weight;
+    //constructor
+    public Edge(int id, Vertex source, Vertex dest, int weight) {
+       this.id = id;
+       this.source = source;
+       this.dest = dest;
+       this.weight = weight;
+   }
+   //getters, usar para correcto uso de OOP
+   public int getId() {
+       return id;
+   }
+   public Vertex getDest() {
+       return dest;
+   }
+   public Vertex getSource() {
+       return source;
+   }
+   public int getWeight() {
+       return weight;
+   }
+   //para imprimir, retorna string "nodoInicial NodoFinal" del arco
+   public String toString() {
+        return source + " " + dest;
+    }
 }
--- Tarea3LP_201873505-1_201873561-2/Estructuras/Pais.java
+++ Tarea3LP_201873505-1_201873561-2/Estructuras/Pais.java
@@ -1,4 +1,33 @@
+import java.util.List;
+import java.util.LinkedList;
+
 class Pais implements Grafo{
+    public void Grafo(int nNodes){
+        Grafo.nNodes = nNodes;
+        adjList =  new LinkedList[nNodes];
+        for(int i = 0; i < adjList.length; i++)
+			adjList[i] = new LinkedList<>();
+	}
+
+    void addEdge(int u, int v, int w){
+        //u - inicio
+        //v - destino
+        //w - peso
+		adjList[u].add(new Edge(v, w));
+        Grafo.nEdges++;
+	}
+    public String toString(){
+		String result="";
+		for(int i = 0;i < adjList.length; i++)
+			result += i + "->" + adjList[i] + "\n";
+		return result;
+	}
+
+    static public void main(String [] args){
+        Grafo g = new Grafo(3);
+        g.addEdge(0,1,3);
+        System.out.println(g.toString());
+    }
     //float edgeWeight(){
 
 //}
