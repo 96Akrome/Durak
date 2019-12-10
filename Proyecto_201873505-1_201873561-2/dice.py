bd93256927f2ec13d1fd2022e8997dbcddb3e5b0
argentum <miffyrli@gmail.com>
2019-12-10 03:20:23 -0300

    Add files via upload

--- Proyecto_201873505-1_201873561-2/dice.py
+++ Proyecto_201873505-1_201873561-2/dice.py
@@ -1,24 +1,25 @@
 from random import randrange
 
+
 def ROLL(dice){
-	if dice == "d4":
-        return randrange(1, 4)
-    
+    if dice == "d4":
+    return randrange(1, 4)
+
     if dice == "d6":
         return randrange(1, 6)
-    
+
     if dice == "d8":
         return randrange(1, 8)
-    
+
     if dice == "d10":
         return randrange(1, 10)
-    
+
     if dice == "d12":
         return randrange(1, 12)
-    
+
     if dice == "d20":
         return randrange(1, 20)
-    
+
     if dice == "d100":
         return randrange(1, 100)
 }
