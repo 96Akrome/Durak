3958b08fa0cc9d90dc2d212653f9d49adbc1ab70
argentum <miffyrli@gmail.com>
2019-12-17 17:42:54 -0300

    lo odio lo odio aaa

--- Tarea4LP_201873505-1_201873561-2/P1.rkt
+++ Tarea4LP_201873505-1_201873561-2/P1.rkt
@@ -2,14 +2,14 @@
 
 ;;(right tree)
 ;;Aplica car(cdr(cdr(tree))) al arbol pasado.
-;;Retorna el valor de car(cdr(cdr(tree))), es decir, subarbol derecho.
+;;Retorna el valor de car(cdr(cdr(tree))), es decir, subarbol derecho
 (define (right tree)
   (caddr (tree))
   )
 
 ;;(left tree)
 ;;Aplica car(cdr(tree)) al arbol pasado.
-;;Retorna el valor de car(cdr(tree)), es decir, subarbol izquierdo.
+;;Retorna el valor de cdr(tree), es decir, subarbol izquierdo.
 (define (left tree)
   (cadr (tree))
   )
@@ -23,8 +23,8 @@
     ((and (empty? arbol1) (empty? arbol2) ) #t)
     ((or (empty? arbol1) (empty? arbol2)) #f)
     (else
-     (and (gemelos (right arbol1) (right arbol2)) (gemelos (left arbol1) (left arbol2))))
+     (and (gemelos (right (arbol1)) (right (arbol2))) (gemelos (left (arbol1)) (left (arbol2)))))
     )
   )
 
-      
+       
