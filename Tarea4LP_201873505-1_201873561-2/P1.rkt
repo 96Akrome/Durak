42d40845606856681cc2a8d37add147ee4520950
argentum <miffyrli@gmail.com>
2019-12-17 17:39:49 -0300

    lo detesto con todo mi corazon

---
+++ Tarea4LP_201873505-1_201873561-2/P1.rkt
@@ -0,0 +1,30 @@
+#lang scheme
+
+;;(right tree)
+;;Aplica car(cdr(cdr(tree))) al arbol pasado.
+;;Retorna el valor de car(cdr(cdr(tree))), es decir, subarbol derecho
+(define (right tree)
+  (caddr (tree))
+  )
+
+;;(left tree)
+;;Aplica car(cdr(tree)) al arbol pasado.
+;;Retorna el valor de cdr(tree), es decir, subarbol izquierdo.
+(define (left tree)
+  (cadr (tree))
+  )
+
+
+(define empty '() )
+
+
+(define (gemelos arbol1 arbol2)
+  (cond
+    ((and (empty? arbol1) (empty? arbol2) ) #t)
+    ((or (empty? arbol1) (empty? arbol2)) #f)
+    (else
+     (and (gemelos (right arbol1) (right arbol2)) (gemelos (left arbol1) (left arbol2))))
+    )
+  )
+
+       
