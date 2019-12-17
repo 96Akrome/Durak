4a1699faa5d736c5288e3a4c875e1e60841df7ef
argentum <miffyrli@gmail.com>
2019-12-17 17:55:21 -0300

    lo odio pero paso varios casos asi que pico uwu

--- Tarea4LP_201873505-1_201873561-2/P1.rkt
+++ Tarea4LP_201873505-1_201873561-2/P1.rkt
@@ -1,17 +1,17 @@
 #lang scheme
 
 ;;(right tree)
-;;Aplica car(cdr(cdr(tree))) al arbol pasado.
+;;Aplica car(cdr(cdr tree)) al arbol pasado.
 ;;Retorna el valor de car(cdr(cdr(tree))), es decir, subarbol derecho
 (define (right tree)
-  (caddr (tree))
+  (caddr tree)
   )
 
 ;;(left tree)
 ;;Aplica car(cdr(tree)) al arbol pasado.
-;;Retorna el valor de cdr(tree), es decir, subarbol izquierdo.
+;;Retorna el valor de car(cdr tree), es decir, subarbol izquierdo.
 (define (left tree)
-  (cadr (tree))
+  (cadr tree)
   )
 
 
@@ -23,7 +23,7 @@
     ((and (empty? arbol1) (empty? arbol2) ) #t)
     ((or (empty? arbol1) (empty? arbol2)) #f)
     (else
-     (and (gemelos (right (arbol1)) (right (arbol2))) (gemelos (left (arbol1)) (left (arbol2)))))
+     (and (gemelos (right arbol1) (right arbol2)) (gemelos (left arbol1) (left arbol2))))
     )
   )
 
