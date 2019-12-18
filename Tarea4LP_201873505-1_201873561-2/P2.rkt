ed0597c21c7f4c2626d2527a4eec412179c20104
argentum <miffyrli@gmail.com>
2019-12-18 14:33:33 -0300

    Add files via upload

--- Tarea4LP_201873505-1_201873561-2/P2.rkt
+++ Tarea4LP_201873505-1_201873561-2/P2.rkt
@@ -1,8 +1,13 @@
 #lang scheme
 
+;; (empty)
+;; Define empty como la lista vacia '()
+;; Retorna la lista vacia
 (define empty '())
 
-
+;;(sum lst)
+;; Suma todos los elementos de la lista lst, usando la recursion de cola simple.
+;;Retorna la suma de todos los elementos de lst.
 (define (sum lst)
   (if (empty? lst)
       0
@@ -10,13 +15,19 @@
       )
   )
 
-(define (min-fn a b) (if (< a b) a b))
-(define (max-fn a b) (if (> a b) a b))
+;;(min-fn a b)
+;; Compara la cola y de la lista a = (x y) con el valor numerico b.
+;;Retorna la cabeza x de la lista a = (x y), en caso de que y es menor a b. En caso contrario, retorna b.
+(define (min-fn a b) (if (<  (cadr a) b) (car a) b))
 
+;;(max-fn a b)
+;; Compara la cola y de la lista a = (x y) con el valor numerico b.
+;;Retorna la cabeza  x de la lista a = (x y), en caso de que y es mayor a b. En caso contrario, retorna b.
+(define (max-fn a b) (if (>  (cadr a) b) (car a) b))
 
-(define (maxmin lst)
-  (let loop ( (x -inf.0) (y +inf.0)  (lst (map (lambda (lst)(list (car lst) (sum (cdr lst)))) lst)))
-    (if (empty? lst)
+(define (maymecola listadelistas)
+  (let loop ( (x -inf.0) (y +inf.0)  (l (map (lambda (listadelistas)(list (car listadelistas) (sum (cdr listadelistas)))) listadelistas)))
+    (if (empty? l)
         (list x y)
-        (loop (max-fn (cadar lst) x) (min-fn (cadar lst) y) (cdr lst))))
+        (loop (max-fn (car l) x) (min-fn (car l) y) (cdr l))))
   )
