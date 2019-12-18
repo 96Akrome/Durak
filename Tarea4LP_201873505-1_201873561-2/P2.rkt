422ffd4b9152af18826f13a96a2234a1ed0cb36a
argentum <miffyrli@gmail.com>
2019-12-18 01:58:19 -0300

    Add files via upload

--- Tarea4LP_201873505-1_201873561-2/P2.rkt
+++ Tarea4LP_201873505-1_201873561-2/P2.rkt
@@ -12,7 +12,25 @@
 
 
 (define (mapear_suma lst)
-(map (lambda (lst) (sum lst)) lst)
+  (map (lambda (lst) (sum lst)) lst)
   )
 ;(mapear_suma '((0 0 9) (1 1 1 1 1)))
 ;(9 5)
+
+(define (maxlst lst)
+  (cond
+    ( empty? lst writeln "Lista vacia, no tiene maximo")
+    ((= (length lst) 1)(car lst))
+    (else max((car lst)(maxlst (cdr lst)))
+          )
+    )
+  )
+
+(define (minlst lst)
+  (cond
+    (empty? lst writeln "Lista vacia, no tiene minimo.")
+    ((= (length lst) 1)(car lst))
+    (else (min((car lst))(minlst (cdr lst)))
+          )
+    )
+  )
