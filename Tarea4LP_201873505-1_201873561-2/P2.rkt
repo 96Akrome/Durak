70d40793b425f0a350e30a54456b057c08028e50
argentum <miffyrli@gmail.com>
2019-12-18 13:28:18 -0300

    Add files via upload

--- Tarea4LP_201873505-1_201873561-2/P2.rkt
+++ Tarea4LP_201873505-1_201873561-2/P2.rkt
@@ -10,19 +10,13 @@
       )
   )
 
-
-(define (mapear_suma lst)
-  (map (lambda (lst) (sum lst)) lst)
-  )
-;(mapear_suma '((0 0 9) (1 1 1 1 1)))
-;(9 5)
-
-;para hacer : pasar la lista por maxlst y minlst, quiza fusionar las 2 funciones, retornar tupla maxsuma, minsuma, buscar su pos en lista actual, retornar cabezas.
+(define (min-fn a b) (if (< a b) a b))
+(define (max-fn a b) (if (> a b) a b))
 
 
 (define (maxmin lst)
   (let loop ( (x -inf.0) (y +inf.0)  (lst (map (lambda (lst)(list (car lst) (sum (cdr lst)))) lst)))
     (if (empty? lst)
         (list x y)
-        (loop (max (car lst) x) (min (car lst) y) (car lst))))
+        (loop (max-fn (cadar lst) x) (min-fn (cadar lst) y) (cdr lst))))
   )
