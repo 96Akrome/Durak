7fd8e2be76dfe62ea5290dc8d31b0c7255a62b00
argentum <miffyrli@gmail.com>
2019-12-18 20:57:14 -0300

    casi casi

--- Tarea4LP_201873505-1_201873561-2/P8.rkt
+++ Tarea4LP_201873505-1_201873561-2/P8.rkt
@@ -6,11 +6,20 @@
       (cons (list (car x)(cadr x)) (pairs (cdr x)))))
 
 
-
-
-
 ;;  (map (lambda (i j) (list i j)) x  x)))
 
+(define (sum lst)
+  (if (empty? lst)
+      0
+      (+ (car lst) (sum (cdr lst)))
+      )
+  )
 
+(define (lolsum listadelistas)
+  (map (lambda (listadelistas) (sum listadelistas)) listadelistas)
+  )
 
+(define (cima lista)
+  writeln "Lol"
+  )
 
