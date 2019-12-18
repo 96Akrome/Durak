061e87728d167b9714604b1151a456c093395648
argentum <miffyrli@gmail.com>
2019-12-18 02:42:40 -0300

    Add files via upload

--- Tarea4LP_201873505-1_201873561-2/P2.rkt
+++ Tarea4LP_201873505-1_201873561-2/P2.rkt
@@ -1,7 +1,6 @@
 #lang scheme
 
-(define empty '()
-  )
+(define empty '())
 
 (define (sum lst)
   (if (empty? lst)
@@ -17,6 +16,7 @@
 ;(mapear_suma '((0 0 9) (1 1 1 1 1)))
 ;(9 5)
 
+;para hacer : pasar la lista por maxlst y minlst, quiza fusionar las 2 funciones, retornar tupla maxsuma, minsuma, buscar su pos en lista actual, retornar cabezas.
 
 (define (maxlst lst)
   (cond
