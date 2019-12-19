b61cbc43dfa21a1d8ef673c4a3d957067a4e206c
argentum <miffyrli@gmail.com>
2019-12-18 21:18:49 -0300

    listo uwu

--- Tarea4LP_201873505-1_201873561-2/P8.rkt
+++ Tarea4LP_201873505-1_201873561-2/P8.rkt
@@ -1,13 +1,20 @@
 #lang scheme
-
+;;(pairs x)
+;;Recibe una lista, denotada como x, y agrupa el elemento i y i+1 en una lista interna.
+;;Retorna lista de pares de numeros consecutivos. Ej: (1 2 3 4)-> (1 2)(2 3)(3 4).
 (define (pairs x)
   (if (or (= 0 (length x))(= 1 (length x)))
       '()
       (cons (list (car x)(cadr x)) (pairs (cdr x)))))
 
+;; (empty)
+;; Define empty como la lista vacia '()
+;; Retorna la lista vacia
+(define empty '())
 
-;;  (map (lambda (i j) (list i j)) x  x)))
-
+;;(sum lst)
+;; Suma todos los elementos de la lista lst, usando la recursion de cola simple.
+;;Retorna la suma de todos los elementos de lst.
 (define (sum lst)
   (if (empty? lst)
       0
@@ -15,11 +22,17 @@
       )
   )
 
+;;(lolsum listadelistas)
+;;Recibe una lista de listas y aplica funcion map, sobre cada lista interna, con funcion de suma como parametro.
+;;Retorna una lista de sumas de las listas internas de la lista original.
 (define (lolsum listadelistas)
   (map (lambda (listadelistas) (sum listadelistas)) listadelistas)
   )
 
+
 (define (cima lista)
-  writeln "Lol"
+  (if (= 1 (length lista))
+      (car lista)
+      (cima (lolsum (pairs lista))))
   )
 
