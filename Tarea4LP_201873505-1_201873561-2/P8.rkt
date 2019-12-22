c1c92510bbb9a77039a586545c13bc34e4237435
argentum <miffyrli@gmail.com>
2019-12-22 01:45:36 -0300

    identacion y orden arreglados

--- Tarea4LP_201873505-1_201873561-2/P8.rkt
+++ Tarea4LP_201873505-1_201873561-2/P8.rkt
@@ -1,19 +1,18 @@
 #lang scheme
 ;;(pairs x)
 ;;Recibe una lista, denotada como x, y agrupa el elemento i y i+1 en una lista interna.
-;;Retorna lista de pares de numeros consecutivos. Ej: (1 2 3 4)-> (1 2)(2 3)(3 4).
+;;Retorna lista de pares de números consecutivos. Ej: (1 2 3 4) -> (1 2)(2 3)(3 4).
 (define (pairs x)
-  (if (or (= 0 (length x))(= 1 (length x)))
+  (if (or
+       (= 0 (length x))
+       (= 1 (length x)))
       '()
-      (cons (list (car x)(cadr x)) (pairs (cdr x)))))
-
-;; (empty)
-;; Define empty como la lista vacia '()
-;; Retorna la lista vacia
-(define empty '())
+      (cons (list (car x) (cadr x)) (pairs (cdr x)))
+      )
+  )
 
 ;;(sum lst)
-;; Suma todos los elementos de la lista lst, usando la recursion de cola simple.
+;;Suma todos los elementos de la lista lst, usando la recursión de cola simple.
 ;;Retorna la suma de todos los elementos de lst.
 (define (sum lst)
   (if (empty? lst)
@@ -23,7 +22,7 @@
   )
 
 ;;(lolsum listadelistas)
-;;Recibe una lista de listas y aplica funcion map, sobre cada lista interna, con funcion de suma como parametro.
+;;Recibe una lista de listas y aplica función map, sobre cada lista interna, con funcion de suma como parámetro.
 ;;Retorna una lista de sumas de las listas internas de la lista original.
 (define (lolsum listadelistas)
   (map (lambda (listadelistas) (sum listadelistas)) listadelistas)
@@ -32,8 +31,9 @@
 
 (define (cima lista)
   (cond
-    ((= 1 (length lista))(car lista))
-    ((= 0 (length lista))(error "Lista vacia!"))
-    (else (cima (lolsum (pairs lista)))))
+    ((= 1 (length lista)) (car lista))
+    ((= 0 (length lista)) (error "Lista vacia!"))
+    (else (cima (lolsum (pairs lista))))
+    )
   )
 
