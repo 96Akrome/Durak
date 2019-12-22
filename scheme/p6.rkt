5b59a39bb22b15ed332a85cba12bfc1ca7b731b4
96 <43505842+96Akrome@users.noreply.github.com>
2019-12-22 14:39:03 -0300

    comentada

--- scheme/p6.rkt
+++ scheme/p6.rkt
@@ -1,16 +1,8 @@
 #lang scheme
 
-"
--Idea: sea k = 5, lista = (1 2 3 4 5 6 7)
-revisar el primer elemento: 1
-conseguir el numero de interes: 5 - 1 = 4
-revisar si el numero de interes esta en la lista: 4 en posicion 3
-agregar par (1 4)
-remover elementos 1 y 4 de la lista
-repetir
-"
-
-;; Devuelve la lista equivalente a list eliminando toda ocurrencia de W y X
+;;(remv W X list).
+;;Busca los numeros W y X en la lista list para remover todas las apariciones de estos en dicha lista.
+;;Retorna la lista equivalente a list eliminando toda ocurrencia de W y X en esta.
 (define remv
   (lambda (W X list)
     (cond
@@ -19,13 +11,18 @@ repetir
       (( eqv? X ( car list)) ( remv W X ( cdr list)))
       (else ( cons (car list) ( remv W X ( cdr list)) )))))
 
-;; Revisa si x existe en la lista list
-(define (member? x ls)
-     (cond ((null? ls) #f)
-           ((equal? x (car ls)) #t)
-           (else   (member? x (cdr ls)))))
+;;(member? x lista).
+;;Revisa si x existe en la lista ls
+;;Retorna #t si x existe en la lista, #f si no.
+(define (member? x lista)
+     (cond ((null? lista) #f)
+           ((equal? x (car lista)) #t)
+           (else   (member? x (cdr lista)))))
 
-;; Entrega la lista con las tuplas de numero que al ser sumados dan el numero k ingresado
+
+;;(aux k lista).
+;;Revisa si en la lista ingresada hay números que al sumarlos den como resultado el número ingresado k para luego imprimir una lista de tuplas con los pares encontrados
+;;Retorna la lista con las tuplas de números que al ser sumados dan el numero k ingresado.
 (define (aux k lista)
   (if (empty? lista)
       '()
@@ -35,19 +32,10 @@ repetir
             (if (member? (- k num) lis)
                 (if (empty? (remv num (- k num) lis))
                     (append tupla (list (list num (- k num))))
-                    (iterar (car (remv num (- k num) lis)) (cdr (remv num (- k num) lis)) (append tupla (list (list num (- k num)))))
-                    )
+                    (iterar (car (remv num (- k num) lis)) (cdr (remv num (- k num) lis)) (append tupla (list (list num (- k num))))))
                 (iterar (car lis) (cdr lis) tupla))))))
 
 (define (armar k lista)
   (if (empty? (aux k lista))
       #f
       (aux k lista)))
-
-
-
-"
-(armar 12 '(1 2 3 8 23 8 14 4 9))
-((8 4) (3 9))
-
-"
