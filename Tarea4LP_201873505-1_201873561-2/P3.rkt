e62891b5a65612f17d9c20b012feaa8a7c3a9601
argentum <miffyrli@gmail.com>
2019-12-22 00:30:34 -0300

    comentarios agregados, hay un bug que voy a solucionar.

--- Tarea4LP_201873505-1_201873561-2/P3.rkt
+++ Tarea4LP_201873505-1_201873561-2/P3.rkt
@@ -1,5 +1,8 @@
 #lang scheme
 
+;;(pvp x y a)
+;;Recibe dos numeros binarios y un tipo de ataque. Segun el ultimo se aplica la operacion binaria a los dos numeros recibidos.
+;;Retorna el valor de retorno de la operacion binaria. 1 si es Verdadero y 0 si es Falso. En caso de ataque incorrecto, se desplega un error.
 (define (pvp x y a)
   (cond
     ((eqv? a 'A)(and x y))
@@ -9,17 +12,27 @@
     )
   )
 
+;;(xor-fn x y)
+;;Aplica la definicion de operacion logica xor a dos numeros binarios.
+;;Retorna 1 si el xor da como resultado Verdadero, en otro caso retorna 0.
 (define (xor-fn x y)
-  (if (boolean=? x y) 0 1)
+  (if ((negate eqv?)x y) 1 0)
   )
 
 
+;;(max-fn a b)
+;;Recibe dos parametros numericos, y decide de los dos es mayor o si se produjo el empate.
+;;Si a > b, se retorna 1, y b > a, 2. En caso de empate, #f.
 (define (max-fn a b)
   (cond
-    ((> a b) 1 2)
+    ((> a b) 1)
+    ((> b a) 2)
     (else #f))
   )
 
+;;(comparar paridad l1 l2 attack)
+;;Recibe el numero-contador paridad, dos listas y el tipo de ataque.
+;;Retorna la lista (1 0) si la primera lista gano y la paridad era par, (0 1) si la segunda y paridad era impar. En otro caso se retorna (0 0).
 (define (comparar paridad l1 l2 attack)
   (cond
    ((eqv? 0 (pvp (car l1) (car l2) attack)) (list 0 0))
@@ -32,17 +45,10 @@
   (let loop ((attack (car lista)) (l1 (cadr lista))(l2 (caddr lista))(paridad 0)(n1 0)(n2 0))
     (if (empty? l1)
         (max-fn n1 n2)
-        (begin
-         (+ n1 (car (comparar paridad l1 l2 attack)))
-         (+ n2 (cadr (comparar paridad l1 l2 attack)))     
-         (loop attack (cdr l1) (cdr l2) (+ paridad 1) n1 n2))
+        (let ( (n1 (+ n1 (car (comparar paridad l1 l2 attack)))) (n2 (+ n2 (cadr (comparar paridad l1 l2 attack)))))
+          (loop attack (cdr l1) (cdr l2) (+ paridad 1) n1 n2))
     )
   )
 )
 
 
-
-  ;; (cond
-    ;; ((and (even? paridad)(pvp (car l1) (car l2) attack) (+ n1 1)) writeln "estoy en par")
-     ;;((and (not(even? paridad))(pvp (car l1) (car l2) attack) (+ n2 1)) writeln "estoy en impar"))
-    ;;(loop attack (cdr l1) (cdr l2) (+ paridad 1) n1 n2)))
