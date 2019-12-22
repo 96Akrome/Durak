f81de47d1b3b0f8d8214c2eb2105bba7bb2876eb
96 <43505842+96Akrome@users.noreply.github.com>
2019-12-22 15:51:33 -0300

    sin <=

--- scheme/p7.rkt
+++ scheme/p7.rkt
@@ -2,6 +2,8 @@
 
 (define fpi
   (lambda (funcion umbral i)
-    (if (<= (abs (- (funcion i) i)) umbral) 
-        0
-        (+ 1 (fpi funcion umbral (funcion i))))))
+    (if (eqv? (abs (- (funcion i) i)) umbral)
+        0        
+        (if (< (abs (- (funcion i) i)) umbral) 
+            0
+            (+ 1 (fpi funcion umbral (funcion i)))))))
