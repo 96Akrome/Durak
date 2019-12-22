63a06254582c109b252b6f19acac3214c9148c69
argentum <miffyrli@gmail.com>
2019-12-22 01:27:27 -0300

    identacion arreglada

--- Tarea4LP_201873505-1_201873561-2/P2.rkt
+++ Tarea4LP_201873505-1_201873561-2/P2.rkt
@@ -1,12 +1,7 @@
 #lang scheme
 
-;; (empty)
-;; Define empty como la lista vacia '()
-;; Retorna la lista vacia
-(define empty '())
-
 ;;(sum lst)
-;; Suma todos los elementos de la lista lst, usando la recursion de cola simple.
+;;Suma todos los elementos de la lista lst, usando la recursión de cola simple.
 ;;Retorna la suma de todos los elementos de lst.
 (define (sum lst)
   (if (empty? lst)
@@ -16,18 +11,30 @@
   )
 
 ;;(min-fn a b)
-;; Compara la cola y de la lista a = (x y) con el valor numerico b.
+;;Compara la cola y de la lista a = (x y) con el valor numérico b.
 ;;Retorna la cabeza x de la lista a = (x y), en caso de que y es menor a b. En caso contrario, retorna b.
-(define (min-fn a b) (if (<  (cadr a) b) (car a) b))
+(define (min-fn a b)
+  (if (< (cadr a) b)
+      (car a) b)
+  )
 
 ;;(max-fn a b)
-;; Compara la cola y de la lista a = (x y) con el valor numerico b.
+;;Compara la cola y de la lista a = (x y) con el valor numérico b.
 ;;Retorna la cabeza  x de la lista a = (x y), en caso de que y es mayor a b. En caso contrario, retorna b.
-(define (max-fn a b) (if (>  (cadr a) b) (car a) b))
+(define (max-fn a b)
+  (if (> (cadr a) b)
+      (car a) b)
+  )
 
 (define (maymecola listadelistas)
-  (let loop ( (x -inf.0) (y +inf.0)  (l (map (lambda (listadelistas)(list (car listadelistas) (sum (cdr listadelistas)))) listadelistas)))
-    (if (empty? l)
-        (list x y)
-        (loop (max-fn (car l) x) (min-fn (car l) y) (cdr l))))
+  (if (empty? listadelistas) (error "Lista de listas ingresada está vacía!")
+      (let loop (
+                 (x -inf.0)
+                 (y +inf.0)
+                 (l (map (lambda (listadelistas) (list (car listadelistas) (sum (cdr listadelistas)))) listadelistas))
+                 )
+        (if (empty? l)
+            (list x y)
+            (loop (max-fn (car l) x) (min-fn (car l) y) (cdr l))))
+      )
   )
