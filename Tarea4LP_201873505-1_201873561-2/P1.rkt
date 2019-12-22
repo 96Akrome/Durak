ddba40a71f2a214c36a1d30eac2288329362f1dd
argentum <miffyrli@gmail.com>
2019-12-22 01:28:57 -0300

    comentarios arreglados

--- Tarea4LP_201873505-1_201873561-2/P1.rkt
+++ Tarea4LP_201873505-1_201873561-2/P1.rkt
@@ -2,28 +2,25 @@
 
 ;;(right tree)
 ;;Aplica car(cdr(cdr tree)) al arbol pasado.
-;;Retorna el valor de car(cdr(cdr(tree))), es decir, subarbol derecho
+;;Retorna el valor de car(cdr(cdr(tree))), es decir, subárbol derecho
 (define (right tree)
   (caddr tree)
   )
 
 ;;(left tree)
 ;;Aplica car(cdr(tree)) al arbol pasado.
-;;Retorna el valor de car(cdr tree), es decir, subarbol izquierdo.
+;;Retorna el valor de car(cdr tree), es decir, subárbol izquierdo.
 (define (left tree)
   (cadr tree)
   )
-
-
-(define empty '() )
-
 
 (define (gemelos arbol1 arbol2)
   (cond
     ((and (empty? arbol1) (empty? arbol2) ) #t)
     ((or (empty? arbol1) (empty? arbol2)) #f)
     (else
-     (and (gemelos (right arbol1) (right arbol2)) (gemelos (left arbol1) (left arbol2))))
+     (and (gemelos (right arbol1) (right arbol2)) (gemelos (left arbol1) (left arbol2)))
+     )
     )
   )
 
