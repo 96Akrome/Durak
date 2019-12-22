e17cb70e1f9697f2b5b220ecb4bb14ce8e234bd4
argentum <miffyrli@gmail.com>
2019-12-22 01:38:01 -0300

    comentarios y orden arreglados

--- Tarea4LP_201873505-1_201873561-2/P9.rkt
+++ Tarea4LP_201873505-1_201873561-2/P9.rkt
@@ -1,22 +1,21 @@
 #lang scheme
 
 ;;(cumplen funcion lista)
-;; Fitra la lista original, usando la funcion filter y de la funcion pasada.
-;; Retorna la lista de elementos tales que, al aplicar la funcion, retornan #t.
+;;Fitra la lista original, usando la funcion filter y de la funcion pasada.
+;;Retorna la lista de elementos tales que, al aplicar la función, retornan #t.
 (define (cumplen funcion lista)
   (filter funcion lista)
   )
 
 ;;(no_cumplen funcion lista)
-;; Fitra la lista original, usando la funcion filter y la negacion de la funcion original.
-;; Retorna la lista de elementos tales que, al aplicar la funcion, retornan #f.
+;;Fitra la lista original, usando la funcion filter y la negación de la función original.
+;;Retorna la lista de elementos tales que, al aplicar la función, retornan #f.
 (define (no_cumplen funcion lista)
   (filter (negate funcion) lista)
   )
 
-
 (define (segm funcion lista)
-  (append (cumplen funcion lista)(no_cumplen funcion lista))
+  (append (cumplen funcion lista) (no_cumplen funcion lista))
   )
   
 
