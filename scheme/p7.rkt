fc507140b017fe1fd809fa7f578febcc3f1fad10
96 <43505842+96Akrome@users.noreply.github.com>
2019-12-22 15:19:01 -0300

    creo que esta lista

--- scheme/p7.rkt
+++ scheme/p7.rkt
@@ -1,12 +1,7 @@
 #lang scheme
 
-(define (fpi funcion umbral i)
-  (let iterar ((abs (lambda (x)(if (> x 0) x (* x -1)))) (num i) (iteraciones 0))
-    (if (<= (abs (- (funcion num) num)) umbral)
-        iteraciones
-        (iterar (lambda (x)(if (> x 0) x (* x -1))) (funcion num) (+ iteraciones 1)))))
-
-
-
-
-
+(define fpi
+  (lambda (funcion umbral i)
+    (if (<= (abs (- (funcion i) i)) umbral) 
+        0
+        (+ 1 (fpi funcion umbral (funcion i))))))
