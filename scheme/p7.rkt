e9998b48cd75789748266eb098e1787946cad625
96 <43505842+96Akrome@users.noreply.github.com>
2019-12-22 15:57:49 -0300

    if con or

--- scheme/p7.rkt
+++ scheme/p7.rkt
@@ -2,8 +2,6 @@
 
 (define fpi
   (lambda (funcion umbral i)
-    (if (eqv? (abs (- (funcion i) i)) umbral)
-        0        
-        (if (< (abs (- (funcion i) i)) umbral) 
-            0
-            (+ 1 (fpi funcion umbral (funcion i)))))))
+    (if (or (eqv? (abs (- (funcion i) i)) umbral) (< (abs (- (funcion i) i)) umbral))
+        0         
+        (+ 1 (fpi funcion umbral (funcion i))))))
