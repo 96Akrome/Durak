83699bd497c190016db955b39de77bd3c0325c3f
argentum <miffyrli@gmail.com>
2019-12-22 01:35:39 -0300

    un captaerror agregado, identacion arreglada

--- Tarea4LP_201873505-1_201873561-2/P10.rkt
+++ Tarea4LP_201873505-1_201873561-2/P10.rkt
@@ -1,8 +1,10 @@
 #lang scheme
 
 (define (serie funcion entero)
-  (if (= entero 0)
-      0
-      (+ (funcion entero) (serie funcion (- entero 1)))
+  (if (< entero 0) (error "Entero ingresado es negativo!")
+      (if (= entero 0)
+          0
+          (+ (funcion entero) (serie funcion (- entero 1)))
+          )
       )
   )
